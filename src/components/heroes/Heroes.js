93cbe554f09782128a2ed0d5468860c9bc516069
Antoine MORENO <57266564+Antoine34170@users.noreply.github.com>
2022-01-09 21:40:32 +0100

    httpRequest ok - next to do is build UI

--- src/components/heroes/Heroes.js
+++ src/components/heroes/Heroes.js
@@ -1,12 +1,33 @@
+import axios from 'axios';
+import { useEffect } from "react"
+
 function Heroes() {
 
+    const httpRequest = () => {
+    }
+    axios.get('https://gateway.marvel.com/v1/public/characters', {
+        params: {
+            ts: 1,
+            apikey: "fb57f21d7f32820f929f39ed0896bf34",
+            hash: "0d38747cce41c0fba94d1df240c6c724"
+        }
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
 
-    
     return (
         <div>
             <h1>Heroes.js works</h1>
         </div>
     )
 }
+
 
 export default Heroes
